7180e938ce76833bbcbef7d78a4d471e1e4979db
pat-s <patrick.schratz@gmail.com>
2020-03-24 18:00:01 +0100

    indent

--- tic.R
+++ tic.R
@@ -14,13 +14,13 @@ if (ci_can_push() && !ci_is_tag()) {
     add_step(step_setup_push_deploy(path = ".", branch = "gh-pages",
       orphan = TRUE))
 
-    # only deploy on master branch
-    if (ci_get_branch() == "master") {
-      get_stage("deploy") %>%
-        add_step(step_do_push_deploy(commit_paths = c(
-          "mlr3.pdf",
-          "mlr3pipelines.pdf",
-          "CNAME"))
-        )
-    }
+  # only deploy on master branch
+  if (ci_get_branch() == "master") {
+    get_stage("deploy") %>%
+      add_step(step_do_push_deploy(commit_paths = c(
+        "mlr3.pdf",
+        "mlr3pipelines.pdf",
+        "CNAME"))
+      )
+  }
 }
