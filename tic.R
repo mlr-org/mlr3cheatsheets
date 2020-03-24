a2fdedef797ee31f0099151bbc1c1c2d90452594
pat-s <patrick.schratz@gmail.com>
2020-03-24 17:05:01 +0100

    update README and rename cheatsheet

--- tic.R
+++ tic.R
@@ -1,5 +1,6 @@
 get_stage("deploy") %>%
   add_code_step(withr::with_dir("mlr3", tinytex::latexmk("mlr3cheatsheet.tex"))) %>%
+  add_code_step(fs::file_move("mlr3/mlr3cheatsheet.pdf", "mlr3.pdf"))) %>%
   add_code_step(writeLines("cheatsheets.mlr-org.com", "CNAME"))
 
 if (ci_can_push() && !ci_is_tag()) {
@@ -10,7 +11,7 @@ if (ci_can_push() && !ci_is_tag()) {
     get_stage("deploy") %>%
       add_step(step_setup_push_deploy(path = ".", branch = "gh-pages",
         orphan = TRUE)) %>%
-      add_step(step_do_push_deploy(commit_paths = c("mlr3/mlr3cheatsheet.pdf",
+      add_step(step_do_push_deploy(commit_paths = c("mlr3.pdf",
         "CNAME")))
   }
 }
