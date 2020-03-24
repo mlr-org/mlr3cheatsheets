f79137892273cf6889a84263f0d9cc6b1afdcbfc
pat-s <patrick.schratz@gmail.com>
2020-03-24 15:53:48 +0100

    use withr::with_dir

--- tic.R
+++ tic.R
@@ -1,6 +1,6 @@
 get_stage("deploy") %>%
-  add_code_step(setwd("mlr3"); tinytex::latexmk("mlr3cheatsheet.tex")) %>%
-  add_code_step(writeLines("cheatsheets.mlr-org.com", "docs/CNAME"))
+  add_code_step(wihtr::with_dir("mlr3", tinytex::latexmk("mlr3cheatsheet.tex"))) %>%
+  add_code_step(writeLines("cheatsheets.mlr-org.com", "CNAME"))
 
 if (ci_can_push() && !ci_is_tag()) {
   get_stage("before_deploy") %>%
@@ -10,6 +10,7 @@ if (ci_can_push() && !ci_is_tag()) {
     get_stage("deploy") %>%
       add_step(step_setup_push_deploy(path = ".", branch = "gh-pages",
         orphan = TRUE)) %>%
-      add_step(step_do_push_deploy(commit_paths = "mlr3/mlr3cheatsheet.pdf"))
+      add_step(step_do_push_deploy(commit_paths = c("mlr3/mlr3cheatsheet.pdf",
+      "CNAME"))
   }
 }
