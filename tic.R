8f0f03cf5371888df534fd2cc2131af4c39288a0
Marc Becker <marcbecker@posteo.de>
2021-07-21 09:45:01 +0200

    copy mlr

--- tic.R
+++ tic.R
@@ -7,11 +7,7 @@ get_stage("script") %>%
   add_code_step({
     files = list.files(".", pattern = "^mlr3.*\\.Rmd$", recursive = TRUE)
     lapply(files, function(file) {
-<<<<<<< HEAD
       out_file = gsub(".Rmd", ".pdf", basename(file))
-=======
-      out_file = file.path("pdf", gsub(".Rmd", ".pdf", basename(file)))
->>>>>>> 27e43795ab6251b945e28544f6400a29458b5bc1
       cheatdown::print_pdf(file, out_file)
     })
   })
@@ -24,9 +20,5 @@ if (ci_get_branch() == "main") {
 
   get_stage("deploy") %>%
     add_code_step(writeLines("cheatsheets.mlr-org.com", "CNAME")) %>%
-<<<<<<< HEAD
-    add_step(step_do_push_deploy(commit_paths = c("CNAME", list.files(pattern = "^mlr3.*\\.pdf"))))
-=======
-    add_step(step_do_push_deploy(commit_paths = c("CNAME", "pdf/")))
->>>>>>> 27e43795ab6251b945e28544f6400a29458b5bc1
+    add_step(step_do_push_deploy(commit_paths = c("CNAME", "mlr3tuning.pdf", "mlr3.pdf", "mlr3pipelines.pdf", "mlrcheatsheet.pdf")))
 }
